c86e178d4ba5d022f985de890d6e3d7d29b6e6c8
Chris Montgomery <chrismont@temple.edu>
2025-04-25 11:03:39 -0400

    fix(routes|metadata): replace usage of deprecated `get_blog_details()`
    I have no idea why I ended up using that function but I will blame the
    WordPress documentation and the fact that `get_blog_details()` is not actually
    marked as deprecated.  The function is, in practice, deprecated:
    
    <https://make.wordpress.org/core/2016/11/04/multisite-focused-changes-in-4-7/>

--- routes/routes.php
+++ routes/routes.php
@@ -3,7 +3,6 @@
 // SPDX-FileCopyrightText: (C) 2024 Temple University <kleinweb@temple.edu>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
@@ -11,7 +10,7 @@ use Kleinweb\Auth\Http\Controllers\SamlAuthController;
 
 Route::group([
     'middleware' => 'web',
-    'prefix' => (get_blog_details()->path ?? '/') . 'sp',
+    'prefix' => (get_site()->path ?? '/') . 'sp',
 ], static function () {
     Route::get('/metadata', [SamlAuthController::class, 'metadata']);
 });
