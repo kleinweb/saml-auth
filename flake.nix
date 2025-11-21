1d68e30db8d05b02625cb85a4d021c288f1ecc99
Chris Montgomery <chrismont@temple.edu>
2025-11-21 10:34:39 -0500

    chore: remove `nixConfig`

--- flake.nix
+++ flake.nix
@@ -44,18 +44,4 @@
           };
         };
     };
-
-  nixConfig = {
-    extra-experimental-features = "nix-command flakes";
-    extra-substituters = [
-      "https://kleintu.cachix.org"
-      "https://dotfield.cachix.org"
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "kleintu.cachix.org-1:NMaPJijd2bOo4ECoHaAbkUBESvPFj4qj5FgPy+6Hg2s="
-      "dotfield.cachix.org-1:b5H/ucY/9PDARWG9uWA87ZKWUBU+hnfF30amwiXiaNk="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
 }
